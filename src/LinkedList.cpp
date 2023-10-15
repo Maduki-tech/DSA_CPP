232970011d0ea422bc24fff82504a1b6e3e5a19b
Maduki-tech <d.schlueter1011@gmail.com>
2023-10-15 09:13:48 +0200

    added comments

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -1,8 +1,14 @@
 #include "LinkedList.h"
 #include <iostream>
 
+/**
+ * @brief creating a empty head with a null pointer
+ */
 LinkedList::LinkedList() { head = nullptr; }
 
+/**
+ * @brief deleting the linked list and removing all the nodes
+ */
 LinkedList::~LinkedList() {
     Node *current = head;
     while (current != nullptr) {
@@ -12,22 +18,39 @@ LinkedList::~LinkedList() {
     }
 }
 
+/**
+ * @brief adding a new node to the linked LinkedList
+ *
+ * Explanation: if the head is null, then the new node will be the head.
+ * Otherwise, the new node will be added to the end of the LinkedList.
+ * @param value
+ */
 void LinkedList::add(int value) {
     Node *newNode = new Node;
     newNode->value = value;
     newNode->next = nullptr;
 
+// Node will be the head
     if (head == nullptr) {
         head = newNode;
     } else {
+        // current is the head
         Node *current = head;
+        // while the next node is not null, keep going
         while (current->next != nullptr) {
+            // current is the next node
             current = current->next;
         }
+        // set last node to the new node
         current->next = newNode;
     }
 }
 
+/**
+ * @brief removing a node from the LinkedList
+ *
+ * @param value the value of the node to be removed
+ */
 void LinkedList::remove(int value) {
     Node *current = head;
     Node *previous = nullptr;
